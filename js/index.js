5815fece9a73d8946cabcbb4e7b1bc38aeabb660
lin-sun <1532946238@qq.com>
2016-06-03 18:36:40 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $(function () {
   var mySwiper=new Swiper('.swiper-container',{
+    autoplay:2000,
     pagination: '.swiper-pagination',
   });
   var v5_swiper=new Swiper('.v5-swipe-banner',{
+    autoplay:2000,
     pagination: '.swiper-pagination',
   })
 })
